9434c6d5a98dd847e632ebe5ae3306e8866b234b
akki1310 <akki.jatt1310@gmail.com>
2021-12-02 03:27:07 +0530

    commit 20

--- src/BMW-GS.php
+++ src/BMW-GS.php
@@ -2,3 +2,11 @@
 include 'header.php';
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <div class="container">
+            <div class="img-wrapper"></div>
+            <div class="desc-wrapper"></div>
+        </div>
